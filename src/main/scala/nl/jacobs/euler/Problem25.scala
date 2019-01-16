f7fd7a748b6c3c728f2f938057d92443e112ebe8
jpmjacobs <paul.jacobs1@ing.com>
2019-01-16 12:14:00 +0100

    problem 25

--- src/main/scala/nl/jacobs/euler/Problem25.scala
+++ src/main/scala/nl/jacobs/euler/Problem25.scala
@@ -1,15 +1,11 @@
 package nl.jacobs.euler
 
-object Problem25 extends App with Timer{
-  lazy val stream: Stream[Int] = 0 #:: stream.scanLeft(1)(_ + _)
-  
-  
-  //b
-  
-  
-//  val (result, time) = profile { 
-//    stream.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum 
-//    }
-//  println(s"result:[$result] took [$time] ms")
-  
+object Problem25 extends App with Timer {
+  lazy val stream: Stream[BigInt] = BigInt(0) #:: stream.scanLeft(BigInt(1))(_ + _)
+
+  val (result, time) = profile {
+    stream.indexOf(stream.filter(_.toString().length() > 999).head)
+  }
+
+  println(s"result:[$result] took [$time] ms")
 }
