ea513d9990e59342a232d56c1666f49aaf976d49
jpmjacobs <jpmjacobs@gmail.com>
2019-01-16 10:58:46 +0100

    signed

--- src/main/scala/nl/jacobs/euler/Problem25.scala
+++ src/main/scala/nl/jacobs/euler/Problem25.scala
@@ -3,6 +3,10 @@ package nl.jacobs.euler
 object Problem25 extends App with Timer{
   lazy val stream: Stream[Int] = 0 #:: stream.scanLeft(1)(_ + _)
   
+  
+  //b
+  
+  
 //  val (result, time) = profile { 
 //    stream.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum 
 //    }
