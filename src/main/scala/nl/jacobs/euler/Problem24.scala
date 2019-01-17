79e20e7961cea93b7cf03f5df9170fbfcc2a1ea2
jpmjacobs <paul.jacobs1@ing.com>
2019-01-17 10:41:50 +0100

    24 skeleton

--- src/main/scala/nl/jacobs/euler/Problem24.scala
+++ src/main/scala/nl/jacobs/euler/Problem24.scala
@@ -1,5 +1,8 @@
 package nl.jacobs.euler
 
-object Problem24 extends App {
-  
+object Problem24 extends App with Timer {
+  val (result,time) = profile {
+    //TODO
+  }
+  println(s"result:[$result] took [$time] ms")
 }
