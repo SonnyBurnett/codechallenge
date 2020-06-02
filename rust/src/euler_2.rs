d842fc60bf16e0827db71a8a553f6dddcbf242f9
Quinten Krijger <qkrijger@gmail.com>
2020-06-02 16:06:57 +0200

    rust ex. 1 and ex. 2

--- rust/src/euler_2.rs
+++ rust/src/euler_2.rs
@@ -1,4 +1,5 @@
-fn main() {
+#[allow(dead_code)]
+pub fn run() {
     // the sum will be in the order of 10M so it's safe to use u32, which is in order of 4G
     let mut sum: u32 = 0;
     let mut fibs: [u32; 3] = [0, 1, 2];
@@ -8,7 +9,7 @@ fn main() {
         fibs = next_3_fibonacci_steps(fibs)
     }
 
-    println!("Sum of even fibonacci numbers < 4M: {}", sum);
+    println!("Euler 2, sum of even fibonacci numbers < 4M: {}", sum);
 }
 
 // the sum of an uneven and even number is uneven, of two unevens even
@@ -19,4 +20,19 @@ fn next_3_fibonacci_steps(former: [u32; 3]) -> [u32; 3] {
     let next_1 = former[2] + next_0;
     let next_2 = next_0 + next_1;
     [next_0, next_1, next_2]
+}
+
+#[cfg(test)]
+mod test_fibonacci_generation {
+    use super::*;
+
+    #[test]
+    fn a_set_of_3_fib_numbers_is_calculated_correctly() {
+        assert_eq!(next_3_fibonacci_steps([1, 2, 3]), [5, 8, 13]);
+    }
+
+    #[test]
+    fn input_does_not_have_to_be_valid_fibonacci_numbers_or_sequence() {
+        assert_eq!(next_3_fibonacci_steps([7, 11, 0]), [11, 11, 22]);
+    }
 }
---
+++ rust/src/main.rs
@@ -0,0 +1,6 @@
+mod euler_1;
+mod euler_2;
+
+fn main() {
+  euler_1::run();
+}
