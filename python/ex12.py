fb53af2fe71ca4da73ad1ba5d21cac44836349f5
Quinten Krijger <qkrijger@gmail.com>
2020-06-02 17:27:39 +0200

    document naturals generator

--- python/ex12.py
+++ python/ex12.py
@@ -2,4 +2,15 @@ import itertools
 
 
 def naturals():
+    """
+    Generator for natural numbers.
+
+    I chose for itertools.count instead of writing that function myself, which would be something like:
+
+    def naturals():
+        x = 0
+        while True:
+            yield x
+            x += 1
+    """
     return itertools.count(1, 1)
