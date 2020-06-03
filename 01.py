6f126c2b2418f0886780c7f7e5a3da1a6ec2e45f
Damir Varga <damir.work@outlook.com>
2020-06-03 16:52:00 +0200

    resolved 01-05

---
+++ 01.py
@@ -0,0 +1,7 @@
+#https://projecteuler.net/problem=1
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+#using list comprehension to iterate up to 1000
+#and check for numbers that match criteria mentioned above
+print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
