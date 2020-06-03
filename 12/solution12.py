312eb073915481c77d77546a5d595710cb9ad47c
Peter.Eichelsheim <peter.eichelsheim@ing.com>
2020-06-03 20:04:51 +0200

    problem 12

---
+++ 12/solution12.py
@@ -0,0 +1,21 @@
+def isprime(i):
+    if len([x for x in range(2,i) if i % (x) == 0 and i != x]) == 0:
+        return True
+    else:
+        return False
+
+def create_triangle_number_list(max_triangle_number):
+    triangle_number_list = [1]
+    for addition in range(2, max_triangle_number):
+        triangle_number_list.append(triangle_number_list[len(triangle_number_list)-1]+addition)
+    return triangle_number_list
+
+def number_of_divisors(triangle_number):
+    return len([divisor for divisor in range(1,triangle_number+1) if triangle_number % divisor == 0])
+
+for triangle_nr in create_triangle_number_list(100000):
+    if not isprime(triangle_nr):
+        divisors = number_of_divisors(triangle_nr)
+        if divisors > 200:
+            print("\t found at: {}".format(triangle_nr))
+            print("\t Number of divisors: {}".format(divisors))
