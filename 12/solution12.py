9b3c37738adb41817d7eeb5d15cad704dfc970ef
Peter.Eichelsheim <peter.eichelsheim@ing.com>
2020-06-05 15:47:53 +0200

    ditched the math dependency ;-)

--- 12/solution12.py
+++ 12/solution12.py
@@ -1,6 +1,3 @@
-import math
-
-
 def create_triangle_number_list(max_triangle_number):
     triangle_number_list = [1]
     for addition in range(2, max_triangle_number):
@@ -11,7 +8,7 @@ def create_triangle_number_list(max_triangle_number):
 def number_of_divisors(number):
     # Only iterate till the square-root and multiple number of elements by 2
     # since a*b = b*a
-    return 2 * len([divisor for divisor in range(1, round(math.sqrt(number) + 1)) if number % divisor == 0])
+    return 2 * len([divisor for divisor in range(1, round(number**0.5)+1) if number % divisor == 0])
 
 
 def print_and_stop_after_first_divisor_limit_found(number, divisor_limit):
