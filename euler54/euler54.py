316c8d90f4d3e22b670027e094c5a4480dd9edeb
Vladislav Sadykov <vladislav.sadykov@ing.com>
2020-06-06 00:27:03 +0200

    Reading the game complete

--- euler54/euler54.py
+++ euler54/euler54.py
@@ -1,9 +1,23 @@
 import os
 
-entries = os.listdir(".")
+#entries = os.listdir(".")
+#print(entries)
+
+with open("euler54/p054_poker.txt") as f:
+
+    for line in f.readlines():
+        game = line.split()
+        
+        pl1 = game[0:5]
+        pl2 = game[5:10]
+
+        print("Player 1: " + str(pl1))
+        print("Player 2: " + str(pl2))
+        print("-------------------------")
+
+    
 
 
-#f = open("p054_poker.txt", "r")
-#print(f.readline())
+
 
 
