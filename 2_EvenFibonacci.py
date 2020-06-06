677d010f7352eed6eccc9679a61bd894ce5f5f1a
Gerben <GTBoon.nl@gmail.com>
2020-06-06 19:52:57 +0200

    tidied up the code

--- 2_EvenFibonacci.py
+++ 2_EvenFibonacci.py
@@ -1,16 +1,20 @@
-total=0
-fib1=1
-fib2=2
-fibonacci=[fib1,fib2]
-evens=[fib2]
-while fib1+fib2<4000000:
-#while fib<4000:
-  fib_new=fib1+fib2
-  fib1=fib2
-  fib2=fib_new
-  fibonacci.append(fib_new)
-  if(fib_new%2==0):
-    evens.append(fib_new)
+def calculate_fibonacci(max):
+  fib1=1
+  fib2=2
+  fibonacci=[fib1,fib2]
+  while fib1+fib2<max:
+    fib_new=fib1+fib2
+    fib1=fib2
+    fib2=fib_new
+    fibonacci.append(fib_new)
+  return(fibonacci)
 
-#print(fibonacci)
-print(sum(evens))
+def evens_from_list(MixedList):
+  evensList=[x for x in MixedList if x % 2 == 0]
+  return(evensList)
+
+fibonacci=calculate_fibonacci(4000000)
+evens = evens_from_list(fibonacci)
+
+print("The sum of all evens in the list of Fibonacci numbers below 4 million equals "
+  + str(sum(evens)))
