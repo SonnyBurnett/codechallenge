42c9cf5bd9ddd707bf12c9ac05265175f517fda1
Mike (M.E.) van den Berge <mike.van.den.berge@ing.com>
2020-06-06 17:39:38 +0200

    initial checkin

---
+++ main.py
@@ -0,0 +1,26 @@
+import math
+
+def triangles(n):
+    # see https://nl.wikipedia.org/wiki/Driehoeksgetal
+    return (n * (n + 1) / 2)
+
+def NumberOfDivisors(n):
+    divisors = 0
+    max_range = int(math.ceil(math.sqrt(n)))
+    #slow code for i in range(1, n + 1):
+    for i in range(1, max_range):
+        if n % i == 0:
+            divisors += 2
+    return divisors
+
+div_search = int(input("Number of divisors: "))
+div = 0
+i = 1
+print("Calculating, please standby")
+while True:
+    triangle = triangles(i);
+    div = NumberOfDivisors(triangle)
+    if div >= div_search:
+        break
+    i += 1
+print("Found triangle: " + str(int(triangle)))
