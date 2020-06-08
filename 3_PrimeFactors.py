129dcf99e17edc5e48fba3ff6c3bee7b66405e1e
Gerben <GTBoon.nl@gmail.com>
2020-06-08 10:17:46 +0200

    added loop to make sure it works for even numbers too

--- 3_PrimeFactors.py
+++ 3_PrimeFactors.py
@@ -1,4 +1,14 @@
-def prime(max):
+def isEven(max):
+  '''
+  This function reports if the input is even.
+  One argument is accepted, and it should be an integer.
+  '''
+  if max%2==0:
+    return True
+  else:
+    return False
+
+def next_prime(max):
   '''
   This function calculates the first prime factor for the input.
   One argument is accepted, and it should be an integer.
@@ -11,10 +21,15 @@ def prime(max):
 p=1
 max=600851475143
 orig_max=max
-
 primes=[]
+
+even=isEven(max)
+while even==True:
+  max=int(max/2)
+  primes.append(2)
+  even=isEven(max)
 while p<max:
-  p=prime(max)
+  p=next_prime(max)
   max=int(max/p)
   primes.append(p)
 
