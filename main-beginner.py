7778be4610fae1db58eb494b629b7a9ab6ec328e
Mike (M.E.) van den Berge <mike.van.den.berge@ing.com>
2020-06-09 12:55:20 +0200

    Refactor directory structure

--- main-beginner.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: Mike van den Berge
-Date: 07-Jun-2020
-'''
-sum_of_multiples = 0
-for i in range(1,1000):
-    if ((i % 3) == 0) or ((i % 5)==0):
-        sum_of_multiples += i
-print("Sum of multiples of 3 or 5 below 999: " + str(sum_of_multiples))
