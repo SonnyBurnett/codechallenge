1c8c16ee8d728a856d78ccef7c06e1364b1b597e
Gerben <GTBoon.nl@gmail.com>
2020-06-10 18:04:07 +0200

    restructured code of problem1 and added unittests

--- Problem3/test_PrimeFactors.py
+++ Problem3/test_PrimeFactors.py
@@ -2,8 +2,8 @@ import unittest
 #PrimeFactors = __import__('3_PrimeFactors')
 from PrimeFactors import get_prime_factors
 
-class TestPrimeFactors(unittest.TestCase):
-    def test_factors(self):
+class TestFunction(unittest.TestCase):
+    def test_function(self):
         self.assertEqual(get_prime_factors(16), [2, 2, 2, 2])
         self.assertEqual(get_prime_factors(51), [3, 17])
 
