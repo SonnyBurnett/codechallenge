90ded10e22a3423c4d2aa2aaa3686925e266b56c
Quinten Krijger <qkrijger@gmail.com>
2020-06-13 20:15:07 +0200

    make sure we process the exact nr of lines

--- wk-2/test_solve.py
+++ wk-2/test_solve.py
@@ -6,7 +6,13 @@ def testProcessFileLines():
     expectedLines = ["8C TS KC 9H 4S 7D 2S 5D 3S AC\n",
                      "5C AD 5D AC 9C 7C 5H 8D TD KS\n"]
 
+    nrOfLineAssertionsMade = 0
+
     def stubFunc(line):
+        nonlocal nrOfLineAssertionsMade
+        nrOfLineAssertionsMade += 1
         assert line == expectedLines.pop(0)
 
     solve.processFileLines(stubFunc, 'test_input.txt')
+
+    assert nrOfLineAssertionsMade == 2
