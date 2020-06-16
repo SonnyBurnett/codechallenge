66c45d2411d64b6109946022a7dba6463594097e
XY56RE <xavier.mayeur@ing.com>
2020-06-16 14:08:37 +0200

    Add comments

--- euler54.py
+++ euler54.py
@@ -8,18 +8,34 @@ ranks = [(1, 1, 1, 1, 1), (2, 1, 1, 1), (2, 2, 1), (3, 1, 1), (), (), (3, 2), (4
 
 
 def get_rank(hand):
-    score = list(zip(*sorted(((v, values[k]) for
-                              k, v in Counter(x[0] for x in hand).items()), reverse=True)))
+    # counter returns a dictionary where the key is card face-value and
+    # the value the number of occurrences
+    # e.g. [(1, 2), (2, 9),...] 1 time a "2" value, 2 times a "9" value
+    counter = Counter(x[0] for x in hand)
 
+    # remap the tuples to have one item with the occurrence and one
+    # item with the values --> (1,2,1,1), (2, 9, 6, 13, 14)
+    hand_tuple = [(num, values[key]) for key, num in counter.items()]
+
+    # sort and merge so we obtain a tuple with the rank and with the value
+    # by decreasing order, so we can manage rank tie
+    # --> (2, 1, 1, 1), (14, 13, 9, 6, 2)
+    # and finally convert the ranks into score value
+    score = list(zip(*sorted(hand_tuple, reverse=True)))
     score[0] = ranks.index(score[0])
+
+    # And here some specific handling for straights
     if len(set(card[1] for card in hand)) == 1:
         score[0] = 5  # flush
     if score[1] in straights:
         score[0] = 8 if score[0] == 5 else 4  # str./str. flush
+
+    # return the score tuple
     return score
 
 
 def main():
+    # Compare the scores and count player1 won hands
     player1_win = sum(get_rank(hand[:5]) > get_rank(hand[5:]) for hand in hands)
     print(f"P1 wins {player1_win}")
 
