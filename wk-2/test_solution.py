cd8b75b7a6b2e7a2237049ad64a3d778ad4052a0
Quinten Krijger <qkrijger@gmail.com>
2020-06-18 13:45:05 +0200

    finish refactor of solve to solution

--- wk-2/test_solution.py
+++ wk-2/test_solution.py
@@ -1,4 +1,4 @@
-import solve
+import solution
 
 
 def testProcessFileLines():
@@ -13,7 +13,7 @@ def testProcessFileLines():
         nrOfLineAssertionsMade += 1
         assert line == expectedLines.pop(0)
 
-    solve.processFileLines(stubFunc, 'test_input.txt')
+    solution.processFileLines(stubFunc, 'test_input.txt')
 
     assert nrOfLineAssertionsMade == 2
 
@@ -21,105 +21,105 @@ def testProcessFileLines():
 def testLineToHands():
     line = "2C 3C 4D 9D TD 2H JH QS KS AS"
 
-    assert solve.lineToHands(line) == ("2C 3C 4D 9D TD", "2H JH QS KS AS")
+    assert solution.lineToHands(line) == ("2C 3C 4D 9D TD", "2H JH QS KS AS")
 
 
 def testScore():
     royalFlush = "TC JC QC KC AC"
-    assert solve.score(royalFlush) == (10, 0, 0)
+    assert solution.score(royalFlush) == (10, 0, 0)
 
     straightFlush = "2C 3C 4C 5C 6C"
-    assert solve.score(straightFlush) == (9, 4, 0)
-    assert solve.score(straightFlush) < solve.score(royalFlush)
+    assert solution.score(straightFlush) == (9, 4, 0)
+    assert solution.score(straightFlush) < solution.score(royalFlush)
 
     fourOfAKind = "2C 2H 2S 2D TC"
-    assert solve.score(fourOfAKind) == (8, 0, 8)
-    assert solve.score(fourOfAKind) < solve.score(straightFlush)
+    assert solution.score(fourOfAKind) == (8, 0, 8)
+    assert solution.score(fourOfAKind) < solution.score(straightFlush)
 
     fullHouse = "2C 2D 2S 9C 9D"
-    assert solve.score(fullHouse) == (7, 0, 7)
-    assert solve.score(fullHouse) < solve.score(fourOfAKind)
+    assert solution.score(fullHouse) == (7, 0, 7)
+    assert solution.score(fullHouse) < solution.score(fourOfAKind)
 
     flush = "2C 3C 4C 9C TC"
-    assert solve.score(flush) == (6, 8, 7)
-    assert solve.score(flush) < solve.score(fullHouse)
+    assert solution.score(flush) == (6, 8, 7)
+    assert solution.score(flush) < solution.score(fullHouse)
 
     straight = "2C 3C 4C 5C 6D"
-    assert solve.score(straight) == (5, 4, 0)
-    assert solve.score(straight) < solve.score(flush)
+    assert solution.score(straight) == (5, 4, 0)
+    assert solution.score(straight) < solution.score(flush)
 
     threeOfAKind = "5C 5D 5S 9C TD"
-    assert solve.score(threeOfAKind) == (4, 3, 8)
-    assert solve.score(threeOfAKind) < solve.score(straight)
+    assert solution.score(threeOfAKind) == (4, 3, 8)
+    assert solution.score(threeOfAKind) < solution.score(straight)
 
     twoPair = "3C 3D 4S 4C TD"
-    assert solve.score(twoPair) == (3, 2, 1)
-    assert solve.score(twoPair) < solve.score(threeOfAKind)
+    assert solution.score(twoPair) == (3, 2, 1)
+    assert solution.score(twoPair) < solution.score(threeOfAKind)
 
     pair = "KC KD 4S 9C TD"
-    assert solve.score(pair) == (2, 11, 8)
-    assert solve.score(pair) < solve.score(twoPair)
+    assert solution.score(pair) == (2, 11, 8)
+    assert solution.score(pair) < solution.score(twoPair)
 
     highCard = "AC 3C 4C 9C TD"
-    assert solve.score(highCard) == (1, 12, 8)
-    assert solve.score(highCard) < solve.score(pair)
+    assert solution.score(highCard) == (1, 12, 8)
+    assert solution.score(highCard) < solution.score(pair)
 
 
 def testScoreComparisonToTwoLevelsDeep():
     straightFlush6 = "2C 3C 4C 5C 6C"
     straightFlush10 = "6C 7C 8C 9C TC"
-    assert solve.score(straightFlush10) > solve.score(straightFlush6)
+    assert solution.score(straightFlush10) > solution.score(straightFlush6)
 
     fourOfAKind2 = "2C 2H 2S 2D TC"
     fourOfAKind3 = "3C 3H 3S 3D TC"
     fourOfAKind3HighCardJacks = "3C 3H 3S 3D JD"
-    assert solve.score(fourOfAKind3) > solve.score(fourOfAKind2)
-    assert solve.score(fourOfAKind3HighCardJacks) > solve.score(fourOfAKind3)
+    assert solution.score(fourOfAKind3) > solution.score(fourOfAKind2)
+    assert solution.score(fourOfAKind3HighCardJacks) > solution.score(fourOfAKind3)
 
     fullHouse2 = "2C 2D 2S 9C 9D"
     fullHouse3 = "3C 3D 3S 9C 9D"
     fullHouse3and10 = "3C 3D 3S TC TD"
-    assert solve.score(fullHouse3) > solve.score(fullHouse2)
-    assert solve.score(fullHouse3and10) > solve.score(fullHouse3)
+    assert solution.score(fullHouse3) > solution.score(fullHouse2)
+    assert solution.score(fullHouse3and10) > solution.score(fullHouse3)
 
     flush10 = "2C 3C 4C 7C TC"
     flush9 = "2C 3C 4C 7C 9C"
     flush9and8 = "2C 3C 4C 8C 9C"
-    assert solve.score(flush10) > solve.score(flush9)
-    assert solve.score(flush9and8) > solve.score(flush9)
+    assert solution.score(flush10) > solution.score(flush9)
+    assert solution.score(flush9and8) > solution.score(flush9)
 
     straight7 = "3C 4C 5C 6C 7D"
     straight6 = "2C 3C 4C 5C 6D"
-    assert solve.score(straight7) > solve.score(straight6)
+    assert solution.score(straight7) > solution.score(straight6)
 
     threeOfAKind5 = "5C 5D 5S 9C TD"
     threeOfAKind6 = "6C 6D 6S 9C TD"
     threeOfAKind6HighCardKing = "6C 6D 6S 9C KD"
-    assert solve.score(threeOfAKind6) > solve.score(threeOfAKind5)
-    assert solve.score(threeOfAKind6HighCardKing) > solve.score(threeOfAKind6)
+    assert solution.score(threeOfAKind6) > solution.score(threeOfAKind5)
+    assert solution.score(threeOfAKind6HighCardKing) > solution.score(threeOfAKind6)
 
     twoPair4 = "3C 3D 4S 4C TD"
     twoPair5 = "3C 3D 5S 5C TD"
     twoPair5and4 = "4C 4D 4S 4C TD"
-    assert solve.score(twoPair5) > solve.score(twoPair4)
-    assert solve.score(twoPair5and4) > solve.score(twoPair5)
+    assert solution.score(twoPair5) > solution.score(twoPair4)
+    assert solution.score(twoPair5and4) > solution.score(twoPair5)
 
     pairK = "KC KD 4S 9C TD"
     pairA = "AC AD 4S 9C TD"
     pairAHighCardJ = "AC AD 4S 9C JD"
-    assert solve.score(pairA) > solve.score(pairK)
-    assert solve.score(pairAHighCardJ) > solve.score(pairA)
+    assert solution.score(pairA) > solution.score(pairK)
+    assert solution.score(pairAHighCardJ) > solution.score(pairA)
 
     highCardT = "2C 3C 4C 9C TD"
     highCardA = "2C 3C 4C 9C AD"
     highCardAHighCard10 = "2C 3C 4C TC AD"
-    assert solve.score(highCardA) > solve.score(highCardT)
-    assert solve.score(highCardAHighCard10) > solve.score(highCardA)
+    assert solution.score(highCardA) > solution.score(highCardT)
+    assert solution.score(highCardAHighCard10) > solution.score(highCardA)
 
 
 def testExerciseTestCases():
-    assert solve.score("5H 5C 6S 7S KD") < solve.score("2C 3S 8S 8D TD")
-    assert solve.score("5D 8C 9S JS AC") > solve.score("2C 5C 7D 8S QH")
-    assert solve.score("2D 9C AS AH AC") < solve.score("3D 6D 7D TD QD")
-    assert solve.score("4D 6S 9H QH QC") > solve.score("3D 6D 7H QD QS")
-    assert solve.score("2H 2D 4C 4D 4S") > solve.score("3C 3D 3S 9S 9D")
+    assert solution.score("5H 5C 6S 7S KD") < solution.score("2C 3S 8S 8D TD")
+    assert solution.score("5D 8C 9S JS AC") > solution.score("2C 5C 7D 8S QH")
+    assert solution.score("2D 9C AS AH AC") < solution.score("3D 6D 7D TD QD")
+    assert solution.score("4D 6S 9H QH QC") > solution.score("3D 6D 7H QD QS")
+    assert solution.score("2H 2D 4C 4D 4S") > solution.score("3C 3D 3S 9S 9D")
