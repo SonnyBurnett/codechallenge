7f10e9f3c065848db1be5710b53a3416f99d7775
Quinten Krijger <qkrijger@gmail.com>
2020-06-23 13:07:07 +0200

    analyse frequency of single character

--- wk-3/solution.py
+++ wk-3/solution.py
@@ -1,2 +1,7 @@
 def freq(chars):
-    return {}
+    freqs = {}
+    for c in chars:
+        if c not in freqs:
+            freqs[c] = 0
+        freqs[c] += 1
+    return freqs
--- wk-3/test_solution.py
+++ wk-3/test_solution.py
@@ -3,3 +3,6 @@ import solution
 
 def test_freq_analysis():
     assert solution.freq([]) == {}
+
+    assert solution.freq([0]) == {0: 1}
+    assert solution.freq([0, 0, 0]) == {0: 3}
