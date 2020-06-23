cd6d3c8a0f80903d200d030037b54c90d396a421
Gerben <GTBoon.nl@gmail.com>
2020-06-23 13:58:08 +0200

    adding Problem41 & unittest

---
+++ Problem41/PandigitalPrime.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+def __isPrime(n: int):
+  if n<=3:
+    return n > 1
+  else:
+    if n%2==0 or n%3==0:
+      return False
+
+  i=5
+  while i*i<=n:
+    if n%i==0 or n%(i+2)==0:
+      return False
+    i+=6
+
+  return True
+
+def calculate_pandigital_prime():
+  digits='987654321'
+  for j in range(len(digits)):
+    generator=list(permutations(digits))
+    for i in generator:
+      if __isPrime(int(''.join(i))):
+        return(''.join(i))
+    digits=digits[1:]
+
+def main():
+  print("The largest pandigital prime is: " + calculate_pandigital_prime())
+
+if __name__ == '__main__':
+    main()
