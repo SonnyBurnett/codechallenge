5164ce484db68cb42af8cb5ad537a0b0de5b33c5
Quinten Krijger <qkrijger@gmail.com>
2020-06-23 14:01:38 +0200

    ability to split chars based on which key char is used on them

--- wk-3/solution.py
+++ wk-3/solution.py
@@ -5,3 +5,12 @@ def freq(chars):
             freqs[c] = 0
         freqs[c] += 1
     return freqs
+
+
+def cyclic_split(chars, n):
+    split = []
+    for _ in range(n):
+        split.append([])
+    for i, c in enumerate(chars):
+        split[i % n].append(c)
+    return split
--- wk-3/test_solution.py
+++ wk-3/test_solution.py
@@ -9,3 +9,10 @@ def test_freq_analysis():
 
     assert solution.freq([0, 3, 7]) == {0: 1, 3: 1, 7: 1}
     assert solution.freq([0, 7, 7, 3, 3, 3, 0, 3, 7, 0]) == {0: 3, 3: 4, 7: 3}
+
+
+def test_split_on_n_cycle():
+    keyLength = 3
+    chars = [1, 2, 3, 4, 5, 6]
+
+    assert solution.cyclic_split(chars, keyLength) == [[1, 4], [2, 5], [3, 6]]
