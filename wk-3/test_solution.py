c0e58dc0cca8ac1cbea00a0b3724b4986a479c1f
Quinten Krijger <qkrijger@gmail.com>
2020-06-23 13:13:30 +0200

    freq analysis on random character

--- wk-3/test_solution.py
+++ wk-3/test_solution.py
@@ -6,3 +6,6 @@ def test_freq_analysis():
 
     assert solution.freq([0]) == {0: 1}
     assert solution.freq([0, 0, 0]) == {0: 3}
+
+    assert solution.freq([0, 3, 7]) == {0: 1, 3: 1, 7: 1}
+    assert solution.freq([0, 7, 7, 3, 3, 3, 0, 3, 7, 0]) == {0: 3, 3: 4, 7: 3}
