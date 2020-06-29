23f13a2f43a5583498a6016a77ce3899b0e59a31
Gerben <GTBoon.nl@gmail.com>
2020-06-29 12:38:21 +0200

    Adding Problem20 & unittests

---
+++ Problem20/test_FactorialSum.py
@@ -0,0 +1,28 @@
+import unittest
+from FactorialDigitSum import calculate_digit_sum, main as FDS_main
+from io import StringIO
+import sys
+
+class TestFunction(unittest.TestCase):
+    def test_main(self):
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
+        FDS_main()
+        sys.stdout = sys.__stdout__
+        self.assertIn("648",capturedOutput.getvalue())
+
+    def test_function(self):
+        self.assertEqual(calculate_digit_sum(2),2)
+        self.assertEqual(calculate_digit_sum(35),8)
+
+    def test_value_errors(self):
+        self.assertRaises(ValueError, calculate_digit_sum, -1)
+        self.assertRaises(ValueError, calculate_digit_sum, 0)
+
+    def test_type_errors(self):
+        self.assertRaises(TypeError, calculate_digit_sum, "Test")
+        self.assertRaises(TypeError, calculate_digit_sum, 3+5j)
+        self.assertRaises(TypeError, calculate_digit_sum, 1,0,1)
+
+if __name__ == '__main__':
+    unittest.main()
