61b78cd7577d40dcfd0e8c97d235135aa0a5a4ac
Damir Varga <damir.work@outlook.com>
2020-07-02 12:28:41 +0200

    more test data for p059

--- damir3/p059.py
+++ damir3/p059.py
@@ -35,33 +35,34 @@ import re
 from itertools import cycle, product
 
 
-class Cipher():
+class CipherDecoder():
   def __init__(self,
                minimum_match_percentage: int = 95,
                minimum_words_to_find: int = 5,
-               minimum_length_words: int = 3,
-               minimum_confidence_factor: int = 65):
+               minimum_length_word: int = 3,
+               minimum_confidence_factor: int = 65,
+               complex_dictionary=True):
     if not isinstance(minimum_match_percentage, int) \
        or not isinstance(minimum_words_to_find, int) \
-       or not isinstance(minimum_length_words, int) \
+       or not isinstance(minimum_length_word, int) \
        or not isinstance(minimum_confidence_factor, int):
       raise TypeError('Minimum values expected as positive integeters.')
     if minimum_match_percentage < 50 or minimum_match_percentage > 100:
       ValueError('minimum_match_percentage expected in range from 50 till 100.')
     if minimum_words_to_find < 1:
       ValueError('minimum_words_to_find expected to be positive integer.')
-    if minimum_length_words < 1:
+    if minimum_length_word < 1:
       ValueError('minimum_length_words expected to be positive integer.')
     if minimum_confidence_factor < 30 or minimum_confidence_factor > 100:
       ValueError('minimum_confidence_factor expected in range from 30 till 100.')
 
     self.__location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     self.__cipher = ''
-    self.__english_dictionary = self.__load_file('p059_words_alpha.txt')
+    self.__english_dictionary = self.__load_file('p059_words_alpha.txt') if complex_dictionary else self.__load_file('p059_words_3000_most_common.txt')
     self.__password_letters = string.ascii_lowercase
     self.__minimum_match_percentage = minimum_match_percentage / 100
     self.__minimum_words_to_find = minimum_words_to_find
-    self.__minimum_length_words = minimum_length_words
+    self.__minimum_length_word = minimum_length_word
     self.__minimum_confidence_factor = minimum_confidence_factor / 100
 
   def __load_file(self, filename):
@@ -76,8 +77,7 @@ class Cipher():
 
   def decode_file_message(self, filename):
     data = self.__load_file(filename)
-    decoded_messsage = self.decode_data(data)
-    return Cipher.sum_ascii_codes(decoded_messsage)
+    return self.decode_data(data)
 
   def decode_data(self, data: str) -> str:
     if not isinstance(data, str):
@@ -86,9 +86,9 @@ class Cipher():
       raise TypeError('Expecting a non-empty string.')
 
     for key in product([c for c in self.__password_letters], repeat=3):
-      output = ''.join(chr(int(x) ^ ord(y)) for (x, y) in zip(data.split(','), cycle(key)))
-      all_words = list(re.findall(r'\b(\w{' + str(self.__minimum_length_words) + r',})\b', output))
-      potential_words = list(re.findall(r'(?:(?=^)|(?<=[,.\s]))([a-zA-Z]{' + str(self.__minimum_length_words) + r',})(?=\b)', output))
+      decoded_data = ''.join(chr(int(x) ^ ord(y)) for (x, y) in zip(data.split(','), cycle(key)))
+      all_words = list(re.findall(r'\b(\w{' + str(self.__minimum_length_word) + r',})\b', decoded_data))
+      potential_words = list(re.findall(r'(?:(?=^)|(?<=[,.\s]))([a-zA-Z]{' + str(self.__minimum_length_word) + r',})(?=\b)', decoded_data))
 
       if len(potential_words) >= self.__minimum_words_to_find and \
          len(potential_words) / len(all_words) >= self.__minimum_confidence_factor:
@@ -96,28 +96,29 @@ class Cipher():
         sum_of_matched_english_words = sum([1 for x in matched_words if x is True])
 
         if sum_of_matched_english_words / len(all_words) > self.__minimum_match_percentage:
-          return output
+          return decoded_data
 
     return ''
 
   @staticmethod
-  def sum_ascii_codes(data: str) -> int:
+  def sum_ascii_codes(data: str):
     if not isinstance(data, str):
       raise TypeError('Expecting a non-empty string.')
+
     if len(data) == 0:
-      raise TypeError('Expecting a non-empty string.')
+      return None
 
     return sum([ord(x) for x in data])
 
   def euler_solution(self):
-    return self.decode_file_message('p059_cipher.txt')
+    return CipherDecoder.sum_ascii_codes(self.decode_file_message('p059_cipher.txt'))
 
 
 def main():
   '''
   main function
   '''
-  _cipher = Cipher()
+  _cipher = CipherDecoder()
   print('Solution for the Euler 059 problem:')
   # prints: 129448
   print(_cipher.euler_solution())
--- damir3/p059_test.py
+++ damir3/p059_test.py
@@ -1,26 +1,36 @@
 import unittest
 from itertools import cycle
-from .p059 import Cipher
+from .p059 import CipherDecoder
 
 
 class Test_p059(unittest.TestCase):
   def test_euler(self):
-    _cipher = Cipher()
-    temp = _cipher.decode_file_message('p059_cipher.txt')
+    _cipher = CipherDecoder()
+    temp = _cipher.euler_solution()
     self.assertEqual(temp, 129448)
 
-  def test_simple_1(self):
-    _cipher = Cipher(minimum_words_to_find=2)
+  def test_euler_with_simple_dictionary_no_match(self):
+    _cipher = CipherDecoder(complex_dictionary=False)
+    temp = _cipher.euler_solution()
+    self.assertNotEqual(temp, 129448)
+
+  def test_euler_with_simple_dictionary_working(self):
+    _cipher = CipherDecoder(complex_dictionary=False, minimum_match_percentage=69)
+    temp = _cipher.euler_solution()
+    self.assertEqual(temp, 129448)
+
+  def test_simple(self):
+    _cipher = CipherDecoder(minimum_words_to_find=2)
     key = 'dam'
     data = 'Hello world is going strong!'
-    encoded = ','.join(str(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
-    output = _cipher.decode_data(encoded)
+    encoded_data = ','.join(str(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
+    output = _cipher.decode_data(encoded_data)
     self.assertEqual(output, data)
 
-  def test_simple_2(self):
-    _cipher = Cipher(minimum_words_to_find=1, minimum_match_percentage=50)
+  def test_too_simple_no_match(self):
+    _cipher = CipherDecoder(minimum_words_to_find=1, minimum_match_percentage=50)
     key = 'bob'
     data = 'Hello world!'
-    encoded = ','.join(str(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
-    output = _cipher.decode_data(encoded)
+    encoded_data = ','.join(str(ord(x) ^ ord(y)) for (x, y) in zip(data, cycle(key)))
+    output = _cipher.decode_data(encoded_data)
     self.assertNotEqual(output, data)
