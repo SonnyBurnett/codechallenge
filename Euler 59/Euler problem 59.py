8b0c75190ff98002d67c2eb245250e1761eddb7d
The10th-Doctor <66074991+The10th-Doctor@users.noreply.github.com>
2020-07-03 17:03:32 +0200

    Update Euler problem 59.py

--- Euler 59/Euler problem 59.py
+++ Euler 59/Euler problem 59.py
@@ -25,41 +25,44 @@ def most_frequent(List):
     countList = [List.count(n) for n in List]
     maxcount = max(countList)
     return List[countList.index(maxcount)]
+    
+def decrypt(text):
+    position1=[]
+    position2=[]
+    position3=[]
+    
+    for i in range(int(len(text))):
+        if i%3 == 0:
+            position1.append(text[i]
+        if i%3 == 1:
+            position2.append(text[i])
+        if i%3 == 2:
+            position3.append(text[i])
+
+    key1 = xor(most_frequent(position1),32)
+    key2 = xor(most_frequent(position2),32)
+    key3 = xor(most_frequent(position3),32)
+    
+    decrypt1 = [xor(i,key1) for i in position1]
+    decrypt2 = [xor(i,key2) for i in position2]
+    decrypt3 = [xor(i,key3) for i in position3]
+    
+    decrypt = ""
+    
+    for i in range(len(decrypt1)):
+        decrypt += chr(decrypt1[i])
+        if i < len(decrypt2):
+            decrypt += chr(decrypt2[i])
+        if i < len(decrypt3):
+            decrypt += chr(decrypt3[i])
+        
+    print(decrypt)
+    print(sum(decrypt1)+sum(decrypt2)+sum(decrypt3))
 
 
 with open('p059_cipher.txt', 'r') as f:
     largetxt = (f.read()).split(",")
 
 largetxt = [int(i) for i in largetxt]
- 
-position1=[]
-position2=[]
-position3=[]
 
-for i in range(int(len(largetxt))):
-    if i%3 == 0:
-        position1.append(largetxt[i])
-    if i%3 == 1:
-        position2.append(largetxt[i])
-    if i%3 == 2:
-        position3.append(largetxt[i])
-
-key1 = xor(most_frequent(position1),32)
-key2 = xor(most_frequent(position2),32)
-key3 = xor(most_frequent(position3),32)
-
-decrypt1 = [xor(i,key1) for i in position1]
-decrypt2 = [xor(i,key2) for i in position2]
-decrypt3 = [xor(i,key3) for i in position3]
-
-decrypt = ""
-
-for i in range(len(decrypt1)):
-    decrypt += chr(decrypt1[i])
-    if i < len(decrypt2):
-        decrypt += chr(decrypt2[i])
-    if i < len(decrypt3):
-        decrypt += chr(decrypt3[i])
-
-print(decrypt)
-print(sum(decrypt1)+sum(decrypt2)+sum(decrypt3))
+decrypt(largetxt)
