d11ef3eb3ffa26b602fcd9aa9147fc29ea7b7548
Taco Bakker <tsbakker65@gmail.com>
2020-07-04 18:50:13 +0200

    improved 41

--- taco/euler41/euler41.py
+++ taco/euler41/euler41.py
@@ -10,6 +10,10 @@ def get_max_prime_from_permutations(n):
     return max([int(x) if sympy.isprime(int(x)) else -1 for x in list(map("".join, permutations(n)))])
 
 
-if __name__ == '__main__':
+def main():
     print(max([get_max_prime_from_permutations(make_string_from_digits(q)) for q in range(2, 10)]))
+
+
+if __name__ == '__main__':
+    main()
 
