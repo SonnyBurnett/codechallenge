e670d727a4082966bb99fe65578d4fa260de23f3
Robert van der Dussen <robert.dussen@gmail.com>
2020-07-05 11:31:37 +0200

    Build acsii conversion function.

--- week3/solutions/solution59.py
+++ week3/solutions/solution59.py
@@ -2,9 +2,12 @@ from itertools import product, cycle
 
 def keyGen():
     a, z = 97, 122
-    for x in product(range(a, z+1), repeat=3):
-        yield x
+    return product(range(a, z+1), repeat=3)
 
 
 def decrypt(message, key):
     return [ x^y for x, y in zip(message, cycle(key)) ]
+
+
+def convertToAscii(message):
+    return [ chr(x) for x in message ]
--- week3/tests/test59.py
+++ week3/tests/test59.py
@@ -3,7 +3,7 @@ import solutions.solution59 as testSubject
 
 class solution59TestCase(unittest.TestCase):
 
-    def testPasswordKeyGen(self):
+    def testKeyGen(self):
         keygen = testSubject.keyGen()
         
         self.assertIsNotNone(keygen)
@@ -15,3 +15,7 @@ class solution59TestCase(unittest.TestCase):
         eulerExamplesXorKey = [42]
 
         self.assertEqual([107, 65], testSubject.decrypt(eulerExamplesXorMessage, eulerExamplesXorKey))
+
+    def testConvertToAscii(self):
+        eulerExamples = [65, 42, 107]
+        self.assertEqual(['A', '*', 'k'], testSubject.convertToAscii(eulerExamples))
