4660863094333e296b9c8b9277e3d1d8d0bb1056
Robert van der Dussen <robert.dussen@gmail.com>
2020-07-05 12:26:40 +0200

    Added function rate the probability of a text being english.

--- week3/solutions/solution59.py
+++ week3/solutions/solution59.py
@@ -1,11 +1,12 @@
 from itertools import product, cycle
 
+
 def keyGen():
     a, z = 97, 122
     return product(range(a, z+1), repeat=3)
 
 
-def XOR(message, key):
+def xorMessage(message, key):
     return [ x^y for x, y in zip(message, cycle(key)) ]
 
 
@@ -14,4 +15,14 @@ def convertToAscii(message):
 
 
 def decryptMessage(encrypted, key):
-    return ''.join(convertToAscii(XOR(encrypted, key)))
+    return ''.join(convertToAscii(xorMessage(encrypted, key)))
+
+
+def scoreEnglish(text):
+    """Give a text a score based on the occurence of the top 10 most common English words.
+    Based on: https://en.wikipedia.org/wiki/Most_common_words_in_English
+
+    Returns a score as an integer between 0 and 10
+    """
+    top10 = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'it']
+    return sum([1 for x in top10 if x in text.lower().split(' ')])
--- week3/tests/test59.py
+++ week3/tests/test59.py
@@ -10,11 +10,11 @@ class solution59TestCase(unittest.TestCase):
         self.assertEqual((97, 97, 97), next(keygen))
         self.assertEqual((97, 97, 98), next(keygen))
 
-    def testXOR(self):
+    def testXorMessage(self):
         eulerExamplesXorMessage = [65, 107]
         eulerExamplesXorKey = [42]
 
-        self.assertEqual([107, 65], testSubject.XOR(eulerExamplesXorMessage, eulerExamplesXorKey))
+        self.assertEqual([107, 65], testSubject.xorMessage(eulerExamplesXorMessage, eulerExamplesXorKey))
 
     def testConvertToAscii(self):
         eulerExamples = [65, 42, 107]
@@ -25,6 +25,13 @@ class solution59TestCase(unittest.TestCase):
          'Let's explore because it's fun.' It was, 'Let's explore so that we can claim lands for our country,
           so that we can open up new trade routes; let's explore so we can become more powerful.'"""
         converted = [ ord(x) for x in deGrasseIpsum ]
-        encypted = testSubject.XOR(converted, (97, 97, 97))
+        encypted = testSubject.xorMessage(converted, (97, 97, 97))
         
         self.assertEqual(deGrasseIpsum, testSubject.decryptMessage(encypted, (97, 97, 97)))
+
+    def testScoreEnglish(self):
+        deGrasseIpsum = """The history of exploration across nations and across time is not one where nations said,
+         'Let's explore because it's fun.' It was, 'Let's explore so that we can claim lands for our country,
+          so that we can open up new trade routes; let's explore so we can become more powerful.'"""
+        foundTop10EnglishWords = 5
+        self.assertEquals(foundTop10EnglishWords, testSubject.scoreEnglish(deGrasseIpsum))
