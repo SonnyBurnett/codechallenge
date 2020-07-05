96f83d110fb63f7eb9928acfcb04445edd0f0fcb
Robert van der Dussen <robert.dussen@gmail.com>
2020-07-05 11:24:41 +0200

    Added decypt function.

--- week3/solutions/solution59.py
+++ week3/solutions/solution59.py
@@ -1,6 +1,10 @@
-from itertools import product
+from itertools import product, cycle
 
-def KeyGen():
+def keyGen():
     a, z = 97, 122
     for x in product(range(a, z+1), repeat=3):
         yield x
+
+
+def decrypt(message, key):
+    return [ x^y for x, y in zip(message, cycle(key)) ]
--- week3/tests/test59.py
+++ week3/tests/test59.py
@@ -4,7 +4,14 @@ import solutions.solution59 as testSubject
 class solution59TestCase(unittest.TestCase):
 
     def testPasswordKeyGen(self):
-        keygen = testSubject.KeyGen()
+        keygen = testSubject.keyGen()
+        
         self.assertIsNotNone(keygen)
         self.assertEqual((97, 97, 97), next(keygen))
         self.assertEqual((97, 97, 98), next(keygen))
+
+    def testDecrypt(self):
+        eulerExamplesXorMessage = [65, 107]
+        eulerExamplesXorKey = [42]
+
+        self.assertEqual([107, 65], testSubject.decrypt(eulerExamplesXorMessage, eulerExamplesXorKey))
