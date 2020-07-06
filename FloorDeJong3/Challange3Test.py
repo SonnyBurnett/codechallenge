1b6f36448b9488ed383802f80458b50d0adda126
Floor de Jong <floor.de.jong@ing.com>
2020-07-06 18:14:54 +0200

    Test fot checking English & counting nr of words english.

--- FloorDeJong3/Challange3Test.py
+++ FloorDeJong3/Challange3Test.py
@@ -2,11 +2,18 @@ import unittest
 
 import Challenge3
 
+
 class TestChallenge3(unittest.TestCase):
 
-    def test_xorDecryptAsciiList(self):
-        key = "abc"
-        greeting = "hello"
+    def test_isEnglish(self):
+        text = "This iweublkzuxcnzhd;g is 12 a small test to check how many words common words are found from the 100."
+        text_jibberish = "djhfiyuvdfiu oiewxMLHUmmn P09WE 8723YRGIaksfibcytdhseta9."
+        self.assertEqual(Challenge3.is_english(text), 9)
+        self.assertEqual(Challenge3.is_english(text_jibberish), 0)
 
-        ascii_greeting = [ord(letter) for letter in greeting]
-        encrypted_greeting = xor_decrypt_ascii_list()
+    def test_isEnglish1(self):
+        text = "This iweublkzuxcnzhd;g is 12 a small test to check how many words common words are found from the 100."
+        text_jibberish = "djhfiyuvdfiu oiewxMLHUmmn P09WE 8723YRGIaksfibcytdhseta9."
+
+        self.assertEqual(Challenge3.is_english1(text, "en"), 18)
+        self.assertEqual(Challenge3.is_english1(text_jibberish, "en"), 0)
