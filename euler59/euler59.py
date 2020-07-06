9ed829184a57462cb51951fc53c8da5ee86463b5
Vladislav <vladislav.sadykov@gmail.com>
2020-07-06 13:00:21 +0200

    First part complete

--- euler59/euler59.py
+++ euler59/euler59.py
@@ -11,7 +11,8 @@ ASCII bytes <> [XOR, key] <> decrypted value
 
 '''
 
-def genkey(message, key) -> bytes:
+def genkey(message, key):
+
 
     keyString = ""
     tmp = 0
@@ -25,35 +26,90 @@ def genkey(message, key) -> bytes:
             keyString += key[tmp]
             tmp +=1
 
-    return(bytes(keyString.encode('utf8')))
+    return keyString
 
 
 def xor_strings(s, t):
     """xor two strings together."""
     if isinstance(s, str):
         # Text strings contain single characters
-        return b"".join(chr(ord(a) ^ ord(b)) for a, b in zip(s, t))
+        return [ord(a) ^ ord(b) for a, b in zip(s, t)]
     else:
+        #print("Not a string")
         # Python 3 bytes objects contain integer values in the range 0-255
-        return bytes([a ^ b for a, b in zip(s, t)])
+        return [a ^ ord(b) for a, b in zip(s, t)]
+
     
 #https://www.mathblog.dk/project-euler-59-xor-encryption/
 
-def analysis(message, keyLenght):
-    maxsize = 0
+def analysis(message):
+    ''' 
+        Input is an array of ASCII characters. 
+    '''
+    
+    charDict = {}
+    for ch in message:
+        if ch not in charDict:
+            charDict[ch] = 1
+        else:
+            charDict[ch] = charDict[ch]+1
+    
+    charDictSorted = {k: v for k, v in sorted(charDict.items(), key=lambda item: item[1], reverse=True)}
 
-    for m in message:
-        if int(m) > maxsize:
-            maxsize = m
-    print(maxsize)
+    
+    for k in charDictSorted.keys():
+        charDictSorted[k] = round(charDictSorted[k]/len(message),2)
 
-    aMessage = [keyLenght][maxsize+1]
-    #print(len(aMessage))
+    return charDictSorted
+
+
+
+
+    
+        
+    
 
 if __name__ == "__main__": 
 
+    """  message = "This is a secret message"
+    key = "test"
+
+    gkey = genkey(message,key)
    
+
+    encryptedtext = xor_strings(message,gkey)
+
+    print(f'Encrypted: {encryptedtext}')
+
+    decreptedtext = xor_strings(encryptedtext,gkey)
     
+
+    print(f'Decrypted: {decreptedtext}')
+
+    print("".join(chr(a) for a in decreptedtext)) """
+    
+
+
+
+    """ output = []
+    for a, b in zip(message,genkey):
+        x = ord(a) ^ ord(b)
+        output.append(x)
+        #print(f'A: {ord(a)} B: {ord(b)} xor: {x}')
+        
+    print(output)
+    
+
+    for a, b in zip(output,genkey):
+        x = a ^ ord(b)
+        #output.append(x)
+        print(f'A: {a} B: {b} xor: {chr(x)}') """
+
+
+
+    
+
+
 
 
 
@@ -68,6 +124,8 @@ if __name__ == "__main__":
         for ind, m in enumerate(message):
             message[ind] = int(m)
         
-        analysis(message,3)
-            
+        #print(message)
+        #print(genkey(message,"god"))
+        decryptedtext = xor_strings(message,genkey(message,"fur"))
+        print("".join(chr(a) for a in decryptedtext))
        
