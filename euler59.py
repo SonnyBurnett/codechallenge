694cfcc0964cd5e2d90a43e7a2b659e165d9a585
XY56RE <xavier.mayeur@ing.com>
2020-07-06 22:41:09 +0200

    Deep Source fixes

--- euler59.py
+++ euler59.py
@@ -30,7 +30,7 @@ def analysis(bmsg, keylength=3):
     # create 3 piles corresponding to the encryption of a character by each key character
     piles = [[0 for x in range(maxsize+1)] for y in range(keylength)]
     # 3-items array for the key
-    key = [0 for x in range(3)]
+    top3 = [0 for x in range(3)]
     i = 0
     # loop on the encrypted message
     for b in bmsg:
@@ -46,10 +46,10 @@ def analysis(bmsg, keylength=3):
     # Assume the space character is the most frequent in a text
     space = ord(' ')
     # and decrypt the key
-    key = [k ^ space for k in key]
+    top3 = [k ^ space for k in top3]
     # get the key as a string
     the_key = ''
-    for k in key:
+    for k in top3:
         the_key += chr(k)
     return the_key
 
@@ -71,10 +71,4 @@ def main():
 
 
 if __name__ == '__main__':
-   # main()
-    txt = "a b  c   d    e     f      g "
-    key = 'aaa'
-    bm = encrypt(txt, key)
-    print(bm)
-    guessed_key = analysis(bm)
-    assert key == guessed_key
+   main()
