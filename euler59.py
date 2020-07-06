a072ec9c44d2854151bf08a6bd32a75de55a12e3
XY56RE <xavier.mayeur@ing.com>
2020-07-06 17:16:16 +0200

    Add Comments

--- euler59.py
+++ euler59.py
@@ -1,3 +1,9 @@
+#
+#
+#
+#
+
+
 def encrypt(message, key):
     encrypted_msg = []
     i = 0
@@ -18,30 +24,42 @@ def decrypt(message, key):
 
 def analysis(bmsg, keylength=3):
     maxsize = int(max(bmsg))
+    # create 3 piles corresponding to the encryption of a character by each key character
     piles = [[0 for x in range(maxsize+1)] for y in range(keylength)]
+    # 3-items array for the key
     key = [0 for x in range(3)]
     i = 0
+    # loop on the encrypted message
     for b in bmsg:
         b = int(b)
         j = i % keylength
-        piles[j][b] = piles[j][b] + 1
+        # count the frequency of each encrypted byte
+        piles[j][b] += 1
+        # and retain the most frequent ones for each key
         if piles[j][b] > piles[j][key[j]]:
             key[j] = b
         i += 1
 
+    # Assume the space character is the most frequent in a text
     space = ord(' ')
-    keys = [k ^ space for k in key ]
+    # and decrypt the key
+    key = [k ^ space for k in key]
+    # get the key as a string
     the_key = ''
-    for k in keys:
+    for k in key:
         the_key += chr(k)
     return the_key
 
 
+# read the encrypted message
 with open('p059_cipher.txt', 'r') as f:
     encrypted = f.read()
-
 bmsg = encrypted.split(',')
+# guess the key
 the_key = analysis(bmsg)
 clear_msg = decrypt(bmsg, the_key)
+# print the decrypted message and see whether is is readable
+print(clear_msg)
+# calculate the requested result
 result = sum(ord(c) for c in clear_msg)
 print(result)
